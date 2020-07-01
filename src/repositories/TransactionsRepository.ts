7b3ab87e45bde1e17f06561e70209394f5389504
Anderson Balena Fernandes <andersonbalenafernandes@gmail.com>
2020-07-01 20:50:47 -0300

    Create and list transactions, using concepts learned on bootcamp nivel 3.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,50 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private getTotalIncome(): number {
+    return this.transactions
+      .filter(trans => trans.type === 'income')
+      .reduce((sum, transaction) => {
+        return (sum += transaction.value);
+      }, 0);
+  }
+
+  private getTotalOutcome(): number {
+    return this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((sum, transaction) => {
+        return (sum += transaction.value);
+      }, 0);
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.getTotalIncome();
+    const totalOutcome = this.getTotalOutcome();
+    const total = totalIncome - totalOutcome;
+
+    const balence = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
+
+    return balence;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    };
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
